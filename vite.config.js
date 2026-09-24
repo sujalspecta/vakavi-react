7f127b96025321b0163853c9013a60cefb635c01
sujalspecta <sujalspectaclicks@outlook.com>
2026-09-24 15:33:23 +0530

    react react-dom package update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from 'path';
 export default defineConfig({ plugins: [react()],
+    resolve: {
+    alias: {
+      // Forces everything in your project to use your root React installations
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+      'react/jsx-runtime': path.resolve(__dirname, './node_modules/react/jsx-runtime'),
+    },
+  },
      css: {
     preprocessorOptions: {
       scss: {
